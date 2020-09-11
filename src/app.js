6927c7d14436160d16999bc0242caa9450f7b271
mailtwogopal <44076731+mailtwogopal@users.noreply.github.com>
2020-09-11 11:49:06 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : true}))
 app.listen(port, () => {
+    console.log('changes for build testing from jenkins');
     console.log('app running on port' + port);
     mongoClient.connect(url, {useNewUrlParser : true}, (error, client) => {
         if(error) {
